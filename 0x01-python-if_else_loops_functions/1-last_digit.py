80186ccf2a2d24e1e5668b87b431e5c87e60f8e0
unclegoki <wilx6471@gmail.com>
2024-01-03 15:00:16 -0800

    Fixed bug in -ve nimbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+last_dig = abs(number) % 10
 if number < 0:
-    last_digit = number % -10
-else:
-    last_digit = number % 10
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6 & last_digit != 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    last_dig = -last_dig
+if last_dig > 5:
+    print(f"Last digit of {number} is {last_dig} and is greater than 5")
+elif last_dig == 0:
+    print(f"Last digit of {number} is {last_dig} and is 0")
+elif last_dig < 6 & last_dig != 0:
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
