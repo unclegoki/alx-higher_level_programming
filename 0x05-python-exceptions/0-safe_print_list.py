75c6dd7ffe4f331fae819cd9b3436a421f03c9ed
unclegoki <wilx6471@gmail.com>
2024-01-24 20:21:52 -0800

    fixed error in - safe list printing;

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,8 +3,8 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     for j in range(x):
         try:
-            print(my_list[m], end="")
-            n += 1
+            print(my_list[j], end="")
+            i += 1
         except IndexError:
             break
         print()
