98aad2eb01170ca9bd913bb788b0e0655bdd23d9
unclegoki <wilx6471@gmail.com>
2024-01-24 20:57:59 -0800

    Fixed bug in task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
+    '''Returns a/b'''
     try:
         divErr = a / b
     except ZeroDivisionError:
         divErr = None
     finally:
-        print("Inside result: {}".format(div))
-    return div
+        print("Inside result: {}".format(divErr))
+    return divErr
