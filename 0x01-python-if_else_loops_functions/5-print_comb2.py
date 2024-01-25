d4580c364ac855d0a211cce566a900cb74729797
unclegoki <wilx6471@gmail.com>
2024-01-25 05:41:35 -0800

    fixed task 5

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for i in range(0, 100):
+    if i < 10:
+        print("0{}".format(i), end=", ")
+    else:
+        if i == 99:
+            print("{}".format(i))
+        else:
+            print("{}".format(i), end=", ")
