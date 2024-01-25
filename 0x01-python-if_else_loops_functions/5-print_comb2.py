1f4bf143114f1f00b3f64201f3fafb6c36ede05d
unclegoki <wilx6471@gmail.com>
2024-01-25 05:48:23 -0800

    Made the program again with just 2 print methods

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i < 10:
-        print("0{}".format(i), end=", ")
-    else:
-        if i == 99:
-            print("{}".format(i))
-        else:
-            print("{}".format(i), end=", ")
+for x in range(0, 99):
+    print("{:02d}".format(x), end=", ")
+print("{:02d}".format(x+1))
