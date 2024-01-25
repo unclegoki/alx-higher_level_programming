0a372eac8497d27084a34211cb61103c46590eaf
unclegoki <wilx6471@gmail.com>
2024-01-25 05:11:54 -0800

    fixed task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) != 'q' and chr(i) != 'e':
-        print(f"{chr(i)}", end="")
+    if (chr(i) == 'e') or (chr(i) == 'q'):
+        continue
+    print(f"{chr(i)}", end="")
